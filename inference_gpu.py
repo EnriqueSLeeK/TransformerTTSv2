67a14ad42cfcecb4fe48368fea8a431e761c5e79
ensebast <ensebast@student.42sp.org.br>
2023-11-25 16:30:15 -0300

    .

--- inference_gpu.py
+++ inference_gpu.py
@@ -1,7 +1,9 @@
 
 import torch
 import json
+import librosa
 import os
+import matplotlib.pyplot as plt
 
 import model.build as builder
 
@@ -18,7 +20,21 @@ def inference(model):
         model.model.load_state_dict(data['model_state_dict'])
 
     mel = model("Hello world!")
-    print(mel)
+    mel = mel.transpose(0, 2, 1)[0]
+
+    fig, ax = plt.subplots()
+    img = librosa.display.specshow(mel, x_axis='time',
+
+                            y_axis='mel', sr=22050,
+
+                            fmax=8000, ax=ax)
+
+    fig.colorbar(img, ax=ax, format='%+.3f dB')
+
+    ax.set(title='Mel-frequency spectrogram')
+
+    fig.savefig("mel_fig2.png")
+
 
 
 def main(config):
